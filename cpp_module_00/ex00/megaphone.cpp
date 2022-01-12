0eb3f896b15a0bdcbc746127a01e470fa3395d5e
pthomas-44 <pthomas@student.42lyon.fr>
2022-01-12 17:08:40 +0100

    ex00 done

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 15:27:23 by pthomas           #+#    #+#             */
+/*   Updated: 2022/01/12 17:03:45 by pthomas          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cctype>
+#include <iostream>
+
+void	convert_to_uppercase(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (i < argc)
+		{
+			convert_to_uppercase(argv[i]);
+			std::cout << argv[i];
+			i++;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
