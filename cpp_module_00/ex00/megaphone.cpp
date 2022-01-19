4e82e8f6e5932ad5c0df4ef8a5b512ef314bda38
pthomas-44 <pthomas@student.42lyon.fr>
2022-01-19 16:04:49 +0100

    ex00 update

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,41 +6,39 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 15:27:23 by pthomas           #+#    #+#             */
-/*   Updated: 2022/01/19 14:33:56 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/01/19 15:49:46 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cctype>
 #include <iostream>
 
-void	convert_str_to_uppercase( char *str )
-{
-	size_t	i;
+void	convert_str_to_uppercase( char *str ) {
 
-	i = 0;
-	while (str[i])
-	{
+	size_t	i = 0;
+
+	while ( str[i] ) {
+
 		str[i] = toupper(str[i]);
 		i++;
 	}
 }
 
-int	main( int argc, char **argv )
-{
-	int	i;
+int	main( int ac, char **av ) {
 
-	i = 1;
-	if (argc == 1)
+	int	i = 1;
+
+	if ( ac == 1 )
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	else
-	{
-		while (i < argc)
-		{
-			convert_str_to_uppercase( argv[i] );
-			std::cout << argv[i];
+	else {
+
+		while ( i < ac ) {
+
+			convert_str_to_uppercase( av[i] );
+			std::cout << av[i];
 			i++;
 		}
 	}
 	std::cout << std::endl;
-	return (0);
+	return (EXIT_SUCCESS);
 }
