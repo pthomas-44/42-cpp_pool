6230c6567c762a2081583b519b8f96778379fd6d
pthomas-44 <pthomas@student.42lyon.fr>
2022-01-21 19:31:04 +0100

    updates

--- cpp_module_00/ex01/Contact.hpp
+++ cpp_module_00/ex01/Contact.hpp
@@ -6,14 +6,13 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 14:54:30 by pthomas           #+#    #+#             */
-/*   Updated: 2022/01/21 18:53:10 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/01/21 19:30:51 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_H
 # define CONTACT_H
 
-# include <cstdlib>
 # include <string>
 
 class Contact {
