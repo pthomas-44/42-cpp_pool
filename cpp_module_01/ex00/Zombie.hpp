d6c4cff80048f776a8eeb1fe5cebde582a37403f
pthomas-44 <pthomas@student.42lyon.fr>
2022-01-31 17:30:41 +0100

    updates

--- cpp_module_01/ex00/Zombie.hpp
+++ cpp_module_01/ex00/Zombie.hpp
@@ -6,13 +6,14 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 17:04:38 by pthomas           #+#    #+#             */
-/*   Updated: 2022/01/31 16:35:06 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/01/31 17:24:52 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_H
 # define ZOMBIE_H
 
+#include <cstdlib>
 #include <string>
 
 class Zombie {
--- cpp_module_01/ex01/main.cpp
+++ cpp_module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 17:04:24 by pthomas           #+#    #+#             */
-/*   Updated: 2022/01/31 17:16:57 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/01/31 17:22:25 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,8 @@ int		main( void ) {
 	size_t  hordeSize = 10;
 
 	theWalkingDead = zombieHorde( hordeSize, "Mine" );
-	for (size_t i = 0; i < hordeSize; i++) {
-
+	for ( size_t i = 0; i < hordeSize; i++ )
 		theWalkingDead[i].announce();
-	}
 	delete [] theWalkingDead;
 	return ( EXIT_SUCCESS );
 }
