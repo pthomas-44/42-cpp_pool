b41003365dfa2687ac9219fc8586c84ff9ff20db
Pierre Thomas <pthomas@student.42lyon.fr>
2022-02-22 16:56:38 +0100

    ex03 done

--- cpp_module_03/ex03/DiamondTrap.cpp
+++ cpp_module_03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 18:07:24 by pthomas           #+#    #+#             */
-/*   Updated: 2022/02/22 16:51:16 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 16:56:33 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ DiamondTrap::DiamondTrap( void )
 DiamondTrap::DiamondTrap( std::string name ) : _name( name )
 {
 	ClapTrap::_name = _name + "_clap_name";
-	std::cout << "_name " << _name << std::endl;
-	std::cout << "ClapTrap::_name " << ClapTrap::_name << std::endl;
-	std::cout << "_hitPoints " << _hitPoints << std::endl;
-	std::cout << "_energyPoints " << _energyPoints << std::endl;
-	std::cout << "_attackDamage " << _attackDamage << std::endl;
+	// std::cout << "_name " << _name << std::endl;
+	// std::cout << "ClapTrap::_name " << ClapTrap::_name << std::endl;
+	// std::cout << "_hitPoints " << _hitPoints << std::endl;
+	// std::cout << "_energyPoints " << _energyPoints << std::endl;
+	// std::cout << "_attackDamage " << _attackDamage << std::endl;
 	std::cout << "New DiamondTrap " << this->_name << " fresh from the factory!" << std::endl;
 	return;
 }
