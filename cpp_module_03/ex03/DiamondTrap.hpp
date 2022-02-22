62b41c6da6c5a1108f2a009b76ed04859dbd64e7
Pierre Thomas <pthomas@student.42lyon.fr>
2022-02-22 17:01:43 +0100

    ex03 done

--- cpp_module_03/ex03/DiamondTrap.hpp
+++ cpp_module_03/ex03/DiamondTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: pthomas <pthomas@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 18:07:22 by pthomas           #+#    #+#             */
-/*   Updated: 2022/02/22 16:49:27 by pthomas          ###   ########lyon.fr   */
+/*   Updated: 2022/02/22 17:01:29 by pthomas          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ class DiamondTrap : public ScavTrap, public FragTrap
 		void			whoAmI( void ) const;
 
 		using			ScavTrap::attack;
-		using			ClapTrap::beRepaired;
-		using			ClapTrap::takeDamage;
 
 		using			FragTrap::_hitPoints;
 		using			ScavTrap::_energyPoints;
